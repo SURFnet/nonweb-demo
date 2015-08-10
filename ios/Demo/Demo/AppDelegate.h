85ceba91d75c1da98d48009874625a1197857f8d
Dániel Zolnai <zoiddani@gmail.com>
2015-08-10 14:38:45 +0200

    Base project with a single view

---
+++ ios/Demo/Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by mdevcon on 10/08/15.
+//  Copyright (c) 2015 SURFnet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
